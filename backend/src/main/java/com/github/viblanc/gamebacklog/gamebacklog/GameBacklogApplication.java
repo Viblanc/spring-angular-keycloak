1b51238115a53afb00fc776fad95fd2b0a64841b
Victor Blanchard <viblanc@proton.me>
2024-03-31 15:30:00 +0200

    first commit

---
+++ backend/src/main/java/com/github/viblanc/gamebacklog/gamebacklog/GameBacklogApplication.java
@@ -0,0 +1,13 @@
+package com.github.viblanc.gamebacklog.gamebacklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBacklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBacklogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/viblanc/gamebacklog/gamebacklog/GameBacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viblanc.gamebacklog.gamebacklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
