5c9e424cab5ac7079b94365498331e324b37eceb
Victor Blanchard <viblanc@proton.me>
2024-04-02 01:12:48 +0200

    first draft for security config

---
+++ backend/src/main/java/com/github/viblanc/gamebacklog/gamebacklog/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.github.viblanc.gamebacklog.gamebacklog.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+    @Value("${app.cors.allowed-origins}")
+    private final List<String> allowedOrigins;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(jwtConfigurer ->
+                    jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter)
+                ))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsCfg = new CorsConfiguration();
+        corsCfg.setAllowedOrigins(allowedOrigins);
+        corsCfg.setAllowCredentials(true);
+        corsCfg.setAllowedMethods(List.of("*"));
+        corsCfg.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource cfgSrc = new UrlBasedCorsConfigurationSource();
+        cfgSrc.registerCorsConfiguration("/**", corsCfg);
+
+        return cfgSrc;
+    }
+}
