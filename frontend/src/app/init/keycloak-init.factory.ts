81ac87b4c5181a2cb86aa0c3d842e932d3f8b9d4
Victor Blanchard <viblanc@proton.me>
2024-04-02 00:44:31 +0200

    setup keycloak for angular

---
+++ frontend/src/app/init/keycloak-init.factory.ts
@@ -0,0 +1,21 @@
+import { KeycloakService } from 'keycloak-angular';
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'game-backlog',
+        clientId: 'gamebacklog-spa',
+      },
+      enableBearerInterceptor: true,
+      bearerPrefix: 'Bearer',
+      bearerExcludedUrls: ['/assets'],
+      initOptions: {
+        pkceMethod: 'S256',
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html',
+      },
+    });
+}
