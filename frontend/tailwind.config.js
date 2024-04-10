01f9358df7efd7e29d12d7eaa44653b45b1bafe1
Victor Blanchard <viblanc@proton.me>
2024-04-11 00:30:34 +0200

    ui: improved display of games in user's list (wip)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateY(10%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        "slide-out": {
+          "0%": { transform: "translateY(0%)", opacity: 1 },
+          "100%": { transform: "translateY(10%)", opacity: 0 },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.3s ease-in",
+        "slide-out": "slide-out 0.2s linear",
+      },
+    },
   },
   plugins: [],
 };
