d1d730814274b13195292bb03c7445912a2c58f1
Victor Blanchard <viblanc@proton.me>
2024-04-14 23:36:37 +0200

    front: fixed hardcoded api url

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -3,35 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { Game } from '../models/game.model';
 import { Observable } from 'rxjs';
 import { UserGame } from '../models/user-game.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  constructor(private http: HttpClient) {}
+  private apiUrl: String;
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+  }
 
   getGames(): Observable<UserGame[]> {
-    return this.http.get<UserGame[]>('http://localhost:9000/api/games');
+    return this.http.get<UserGame[]>(`${this.apiUrl}/games`);
   }
 
   search(query: string): Observable<Game[]> {
-    return this.http.get<Game[]>(
-      `http://localhost:9000/api/games/search?query=${query}`
-    );
+    return this.http.get<Game[]>(`${this.apiUrl}/games/search?query=${query}`);
   }
 
   addGame(game: Game): Observable<Game[]> {
-    return this.http.post<Game[]>('http://localhost:9000/api/games/add', game);
+    return this.http.post<Game[]>(`${this.apiUrl}/games/add`, game);
   }
 
   updateGame(game: UserGame): Observable<UserGame> {
     return this.http.put<UserGame>(
-      `http://localhost:9000/api/games/${game.game.id}`,
+      `${this.apiUrl}/games/${game.game.id}`,
       game
     );
   }
 
   removeGame(gameId: number): Observable<void> {
-    return this.http.delete<void>(`http://localhost:9000/api/games/${gameId}`);
+    return this.http.delete<void>(`${this.apiUrl}/games/${gameId}`);
   }
 }
