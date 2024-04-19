e1c5baf1ca7dd4087a8f79bfd71522dfe6f9cc22
Victor Blanchard <viblanc@proton.me>
2024-04-19 19:02:10 +0200

    test: added placeholders for client id and client secret
    Removed trailing whitespaces and unused imports as well

--- backend/src/test/java/com/github/viblanc/gamebacklog/gamebacklog/configuration/PostgresContainerConfig.java
+++ backend/src/test/java/com/github/viblanc/gamebacklog/gamebacklog/configuration/PostgresContainerConfig.java
@@ -1,9 +1,6 @@
 package com.github.viblanc.gamebacklog.gamebacklog.configuration;
 
-import com.github.viblanc.gamebacklog.gamebacklog.service.IGDBApiService;
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
--- backend/src/test/java/com/github/viblanc/gamebacklog/gamebacklog/objectmother/GameMother.java
+++ backend/src/test/java/com/github/viblanc/gamebacklog/gamebacklog/objectmother/GameMother.java
@@ -16,8 +16,8 @@ public class GameMother {
                 .coverUrl("https://images.igdb.com/igdb/image/upload/t_cover_big_2x/co21ro.jpg")
                 .summary("""
                         A 3D platformer and first Wii entry in the Super Mario franchise, Super Mario Galaxy
-                        sees Mario jump across planets and galaxies with varying items, enemies, geographies 
-                        and gravity mechanics in order to reach his enemy Bowser, who has attacked the Mushroom 
+                        sees Mario jump across planets and galaxies with varying items, enemies, geographies
+                        and gravity mechanics in order to reach his enemy Bowser, who has attacked the Mushroom
                         Kingdom and hijacked Princess Peach's castle with her inside.
                         """)
                 .platforms(List.of(PlatformMother.complete().build()));
